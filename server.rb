370e832f3f9efd190106107045ae194b3e82ea87
Dgibbins <nayr_ryan@hotmail.com>
2017-01-24 17:28:42 -0500

    Stretch complete

--- server.rb
+++ server.rb
@@ -6,6 +6,7 @@ port = 2000
 server = TCPServer.open(host, port)                 # Socket to listen to defined host and port
 puts "Server started on #{host}:#{port} ..."        # Output to stdout that server started
 
+
 loop do                                             # Server runs forever
   client = server.accept                            # Wait for a client to connect. Accept returns a TCPSocket
 
@@ -15,6 +16,36 @@ loop do                                             # Server runs forever
   end
   puts lines                                        # Output the full request to stdout
 
-  client.puts(Time.now.ctime)                       # Output the current time to the client
-  client.close                                      # Disconnect from the client
+  # response = File.read(filename)
+  filename = lines[0].gsub(/GET \//, '').gsub(/\ HTTP.*/, '') #Finds the **** string in"GET /**** HTTP/1.1" when initializing a server
+
+  if File.exists?(filename)
+    response_body = File.read(filename)
+    success_header = []
+    success_header << "HTTP/1.1 200 OK"
+    if filename == "style.css"
+      success_header << "Content-Type: text/css"
+    elsif filename == "mockup.png"
+      success_header << "Content-Type: image/png"
+    else
+      success_header << "Content-Type: text/html" # should reflect the appropriate content type (HTML, CSS, text, etc)
+    end
+    success_header << "Content-Length: #{response_body.length}" # should be the actual size of the response body
+    success_header << "Connection: close"
+    header = success_header.join("\r\n")
+  else
+    response_body = "File Not Found\n" # need to indicate end of the string with \n
+    not_found_header = []
+    not_found_header << "HTTP/1.1 404 Not Found"
+    not_found_header << "Content-Type: text/plain" # is always text/plain
+    not_found_header << "Content-Length: #{response_body.length}" # should the actual size of the response body
+    not_found_header << "Connection: close"
+    header = not_found_header.join("\r\n")
+  end
+
+  response = [header, response_body].join("\r\n\r\n")
+
+  client.puts(response)                       # Output the current time to the client
+  client.close
+                        # Disconnect from the client
 end
